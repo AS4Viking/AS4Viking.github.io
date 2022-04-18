2469947f70d1988e25486e3ca06e7b91ac2aa268
Ajdin Salihović <salihovic.ay@gmail.com>
2022-04-18 02:15:52 +0200

    Adding a particle background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import{Component, OnInit}from '@angular/core';
+
+declare var particlesJS: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+constructor() { }
+
+  ngOnInit() {
+  // https://vincentgarreau.com/particles.js/
+  particlesJS.load('particles-js', 'src/assets/data/particles/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+  });
+  }
 }
+
